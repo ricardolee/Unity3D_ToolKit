942a816bb983ff770ab3461a3a5f2aacaefc4a6f
Pengcheng Li <lpc1983@gmail.com>
2015-07-18 15:36:55 +0800

    Add a test

---
+++ Assets/Scripts/Test/StateEnter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using FSM;
+
+public class StateEnter : MonoBehaviour
+{
+    private FiniteStateMachine fsm = new FiniteStateMachine();
+
+    public enum State
+    {
+        Success,
+        Failure,
+    }
+
+
+    public string state;
+    
+
+    void Awake()
+    {
+        fsm.Init<State>(this);        
+    }
+    
+    void Start()
+    {
+        fsm.ChangeState(State.Success);
+    }
+    
+    [StateBehaviour(state = "Success", on = "Enter")]
+    void ChangeStateToSuccess()
+    {
+        state = State.Success.ToString();
+    }
+
+}
